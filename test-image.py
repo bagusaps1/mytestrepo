e9969ce41b2a69eed9b808622c768bfd60310dc8
bagus setiyono <bagus@honestbank.com>
2024-06-06 16:16:42 +0700

    test commit

---
+++ test-image.py
@@ -0,0 +1,11 @@
+import pytesseract
+from PIL import Image
+
+# Load an image from file
+image = Image.open('image/placeholder.png')
+
+# Use Tesseract to do OCR on the image
+text = pytesseract.image_to_string(image)
+
+# Print the extracted text
+print(text)
